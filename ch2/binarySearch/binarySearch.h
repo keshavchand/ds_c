d5e08f6a3e231126e585b46e752b9e69a2a1527f
Keshav Chand <chandkeshav001@gmail.com>
2022-08-13 18:33:06 +0530

    Recursive Binary Search

--- ch2/binarySearch/binarySearch.h
+++ ch2/binarySearch/binarySearch.h
@@ -13,3 +13,17 @@ int BinarySearch(int *a, int size, int s) {
 
 	return -1;
 }
+
+int BinarySearchRec(int *a, int size, int s) {
+	
+	if (size <= 0) return -1;
+	printf("%d\n", size);
+
+	int start = 0, end = size - 1;
+	int mid = (start + end) / 2;
+	int item = a[mid];
+	if (item == s) return mid;
+	if (item < s) BinarySearchRec(a + mid + 1, size - mid - 1, s);
+	if (item > s) BinarySearchRec(a, size - mid - 1, s);
+	return -1;
+}

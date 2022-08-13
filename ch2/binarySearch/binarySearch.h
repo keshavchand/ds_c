13516874b46c01379fa1e4f275952aae7b3796d4
uwu_rimjhim <100113856+rimjhimshrivastava@users.noreply.github.com>
2022-08-13 17:08:56 +0530

    Recursive Approach added

--- ch2/binarySearch/binarySearch.h
+++ ch2/binarySearch/binarySearch.h
@@ -13,3 +13,27 @@ int BinarySearch(int *a, int size, int s) {
 
 	return -1;
 }
+
+/*edit by rimjhim
+here's a recursive approach for binary search
+during function call in main(), pass arguments as endindex = size-1*/
+int BinarySearchRecursive(int *arr, int endindex, int search_element, int startindex = 0)
+{
+    int mid = (startindex + endindex) / 2;
+    if (arr[mid] == search_element)
+    {
+        return mid;
+    }
+    if (startindex == endindex)
+    {
+        return -1;
+    }
+    if (arr[mid] > search_element)
+    {
+        return BinarySearchRecursive(arr, mid - 1, search_element, startindex);
+    }
+    if (arr[mid] < search_element)
+    {
+        return BinarySearchRecursive(arr, endindex, search_element, mid + 1);
+    }
+}
